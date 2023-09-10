e0299edcc1cfd0537b86c2908b93cc79c80fce0d
Malek Kazal <ghazal.malek@gmail.com>
2023-09-10 11:39:12 -0400

    Hero Section

--- react-portfolio/src/Pages/Home/Hero.jsx
+++ react-portfolio/src/Pages/Home/Hero.jsx
@@ -1,3 +1,26 @@
+import heroImg from "../../assets/hero-img.png";
+
 export default function Hero() {
-  return <h1>Hero Section</h1>;
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Malek</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section--title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section--description">
+            Lorem ipsum dolor sit, amet elit.
+            <br /> Lorem ipsum dolor sit, amet elit.
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src={heroImg} alt="hero-img" />
+      </div>
+    </section>
+  );
 }
