b20246d9128cb7cdc3384484213441e2b71179f4
Malek Kazal <ghazal.malek@gmail.com>
2023-09-23 19:07:36 -0400

    Portfolio Projects Content

--- react-portfolio/src/Pages/Home/MyPortfolio.jsx
+++ react-portfolio/src/Pages/Home/MyPortfolio.jsx
@@ -46,24 +46,26 @@ export default function MyPortfolio() {
                 <h3 className="portfolio--section--title">{item.title}</h3>
                 <p className="text-md">{item.description}</p>
               </div>
-              <p className="text-sm portfolio--link">
-                {item.link}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 20 19"
-                  fill="none"
-                >
-                  <path
-                    d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
-                    stroke="currentColor"
-                    stroke-width="2.66667"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </p>
+              <a href={item.link} target="_blank" rel="noreferrer">
+                <p className="text-sm portfolio--link">
+                  View On GitHub
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 20 19"
+                    fill="none"
+                  >
+                    <path
+                      d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
+                      stroke="currentColor"
+                      stroke-width="2.66667"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </p>
+              </a>
             </div>
           </div>
         ))}
